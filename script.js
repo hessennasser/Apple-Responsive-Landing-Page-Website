8e2718fbb8672197818ad1d37038a68e3750b57c
hessennasser <92742551+hessennasser@users.noreply.github.com>
2022-12-08 14:36:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let mainImg = document.querySelector(".main-img");
+let container = document.querySelector(".container");
+
+function phonesImgs (phone){
+    mainImg.src = phone;
+};
+
+function backgroundColor(color){
+    container.style.background = color
+}
